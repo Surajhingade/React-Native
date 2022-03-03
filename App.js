5433f8a33dcd468be7999a73ba4a88b937638eef
go <hingadesuraj7620@gmail.com>
2022-03-03 12:52:27 +0530

    Start expo for the development

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello suraj welcome to react native</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +17,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+ 
 });
